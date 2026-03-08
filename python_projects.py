417251df0c4e26862c50501ee7edcbadb1bdaaca
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-03-08 23:35:43 +0800

    done and I changed int into float

--- python_projects.py
+++ python_projects.py
@@ -1,3 +1,7 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
