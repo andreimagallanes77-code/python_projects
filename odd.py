6a461520578c24e5da6c6f039f5d1181e1680911
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-03-09 13:07:14 +0800

    i changed equal sign into ! because equal sign is counting even

--- odd.py
+++ odd.py
@@ -3,7 +3,7 @@ odd_count = 0
 for count in range(10):
     number = int(input("Enter a number: "))
 
-    if number % 2 == 0:
+    if number % 2 != 0:
         odd_count = odd_count + 1
 
 print("Total odd numbers:", odd_count)
